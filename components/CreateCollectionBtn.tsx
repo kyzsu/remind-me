22f53bcc233a5d20860a63cb85a139001bc54f2d
Immanuel Williams <l0calh34rtz@gmail.com>
2023-08-31 00:12:13 +0700

    feat: implement * till CreateCollectionBtn

---
+++ components/CreateCollectionBtn.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React from "react"
+import { Button } from "./ui/button"
+
+function CreateCollectionBtn() {
+  return (
+    <Button
+      variant={"outline"}
+      className="dark:text-white w-full dark:bg-neutral-950 bg-white"
+    >
+      <span className="bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent">
+        Create Collection
+      </span>
+    </Button>
+  )
+}
+
+export default CreateCollectionBtn
